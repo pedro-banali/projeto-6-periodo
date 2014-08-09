4c5be0bc3b9c90f49b145eb1a1ea005e50f37c55
pedro <pedro@10.97.247.100>
2014-08-09 12:33:16 -0300

    Bagagem

--- TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/model/Bagagem.java
+++ TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/model/Bagagem.java
@@ -5,4 +5,19 @@ public class Bagagem {
 	
 	private long id;
 	private double peso;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	
 }
