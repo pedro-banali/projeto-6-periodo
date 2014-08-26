0d1e4e843d5117c5836d36684b746f91ba018880
gabriel.goes <gabriel.goes@LCET1125.cwbdis.pucpr.br>
2014-08-26 17:08:36 -0300

    Classe Endereco DAO

---
+++ TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/dao/EnderecoDAO.java
@@ -0,0 +1,26 @@
+package br.pucpr.bsi.prog6.ticketsAereosBSI.dao;
+
+import java.util.List;
+
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Endereco;
+
+public class EnderecoDAO extends PatternDAO<Endereco> {
+
+	private static final long serialVersionUID = 1L;
+	private final static EnderecoDAO instance = new EnderecoDAO();
+
+	private EnderecoDAO() {
+		super(Endereco.class);
+	}
+
+	public static EnderecoDAO getInstance() {
+		return instance;
+	}
+
+	@Override
+	public List<Endereco> findByFilter(Endereco filter) {
+		
+			return null;
+		
+	}
+}
