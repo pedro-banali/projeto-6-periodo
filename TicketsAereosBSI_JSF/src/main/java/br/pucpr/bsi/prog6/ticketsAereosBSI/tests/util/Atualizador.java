5307bf76294fab7dcd5de7c9d6bdbcc6f07b1dbc
pedro <pedro@10.97.250.152>
2014-08-26 18:01:26 -0300

    Util

--- TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/tests/util/Atualizador.java
+++ TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/tests/util/Atualizador.java
@@ -1,6 +1,19 @@
 package br.pucpr.bsi.prog6.ticketsAereosBSI.tests.util;
 
+import java.util.Date;
+
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Aeroporto;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Aviao;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Bagagem;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Bilhete;
 import br.pucpr.bsi.prog6.ticketsAereosBSI.model.CiaAerea;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Endereco;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Funcionario;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Horario;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Papel;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Passageiro;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Pessoa;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Rota;
 
 /**
  * Classe responsavel por atualizar informacoes para update e comparacoes
@@ -11,6 +24,40 @@ public class Atualizador {
 	
 	/**
 	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param usuario
+	 * @param cod
+	 */
+	public static void atualizarAeroporto(Aeroporto usuario, String cod){
+		usuario.setCodigo(cod 		+ usuario.getCodigo());
+		usuario.setNome(cod 		+ usuario.getNome());
+
+		//Atualiza o endereco
+		atualizarEndereco(usuario.getEndereco(), cod);
+	}
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param usuario
+	 * @param cod
+	 */
+	public static void atualizarAviao(Aviao usuario, String cod){
+		usuario.setCodigo(cod 		+ usuario.getCodigo());
+		usuario.setCarga(3000 		+ usuario.getCarga());
+	}
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param usuario
+	 * @param cod
+	 */
+	public static void atualizarBilhete(Bilhete usuario, String cod){
+		usuario.setAssento(cod 		+ usuario.getAssento());
+		usuario.setNumero(3000);
+	}
+	
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
 	 * @param ciaAerea
 	 * @param cod
 	 */
@@ -18,4 +65,103 @@ public class Atualizador {
 		object.setNome(cod + object.getNome());
 	}
 	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param endereco
+	 * @param cod
+	 */
+	public static void atualizarEndereco(Endereco endereco, String cod){
+		//Atualiza o endereco
+		endereco.setBairro(cod + endereco.getBairro());
+		endereco.setCidade(cod + endereco.getCidade());
+		endereco.setComplemento(cod + endereco.getComplemento());
+		endereco.setEstado(cod + endereco.getEstado());
+		endereco.setNumero(100000);
+		endereco.setPais(cod + endereco.getPais());
+		endereco.setRua(cod + endereco.getRua());
+	}
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param objeto
+	 * @param cod
+	 */
+	public static void atualizarFuncionario(Funcionario objeto, String cod){
+		objeto.setCodigo(			cod	+ objeto.getCodigo());
+		objeto.setContaCorrente(	cod	+ objeto.getContaCorrente());
+
+		atualizarPessoa(objeto, cod);
+	}
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param objeto
+	 * @param cod
+	 */
+	public static void atualizarHorario(Horario objeto, String cod){
+		objeto.setCodigo(cod 		+ objeto.getCodigo());
+		
+		objeto.setQtdEconomica(objeto.getQtdEconomica() + 10);
+		objeto.setQtdExecutiva(objeto.getQtdExecutiva() + 10);
+		objeto.setQtdPrimeira(objeto.getQtdPrimeira() + 10);
+	}
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param usuario
+	 * @param cod
+	 */
+	public static void atualizarBagagem(Bagagem usuario, String cod){
+		usuario.setPeso(3000);
+	}
+	
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param papel
+	 * @param cod
+	 */
+	public static void atualizarPapel(Papel object, String cod){
+		object.setDescricao(cod + object.getDescricao());
+		object.setNome(cod + object.getNome());
+	}
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param objeto
+	 * @param cod
+	 */
+	public static void atualizarPassageiro(Passageiro objeto, String cod){
+		objeto.setDocumento(	cod	+ objeto.getDocumento());
+		objeto.setNumeroCartao(	cod	+ objeto.getNumeroCartao());
+
+		atualizarPessoa(objeto, cod);
+	}
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param usuario
+	 * @param cod
+	 */
+	public static void atualizarPessoa(Pessoa usuario, String cod){
+		usuario.setDataNascimento(	new Date());
+		usuario.setEmail(cod 		+ usuario.getEmail());
+		usuario.setNome(cod 		+ usuario.getNome());
+		usuario.setSenha(cod 		+ usuario.getSenha());
+		usuario.setTelefone(cod 	+ usuario.getTelefone());
+		usuario.setUsuario(cod 		+ usuario.getUsuario());
+
+		//Atualiza o endereco
+		atualizarEndereco(usuario.getEndereco(), cod);
+	}
+	
+	/**
+	 * Metodo responsavel por atualizar informacoes do objeto
+	 * @param papel
+	 * @param cod
+	 */
+	public static void atualizarRota(Rota object, String cod){
+		object.setDescricao(cod + object.getDescricao());
+		object.setNome(cod + object.getNome());
+	}
 }
--- TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/tests/util/Verificador.java
+++ TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/tests/util/Verificador.java
@@ -2,7 +2,19 @@ package br.pucpr.bsi.prog6.ticketsAereosBSI.tests.util;
 
 import org.junit.Assert;
 
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Aeroporto;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Aviao;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Bagagem;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Bilhete;
 import br.pucpr.bsi.prog6.ticketsAereosBSI.model.CiaAerea;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Endereco;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Funcionario;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Horario;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Papel;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Passageiro;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Pessoa;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.model.Rota;
+import br.pucpr.bsi.prog6.ticketsAereosBSI.utils.DateUtils;
 
 /**
  * Classe responsavel por atualizar informacoes para update e comparacoes
@@ -11,9 +23,114 @@ import br.pucpr.bsi.prog6.ticketsAereosBSI.model.CiaAerea;
  */
 public class Verificador {
 	
+	public static void verificar(Aeroporto objetoBD, Aeroporto objeto){
+		Assert.assertEquals(objetoBD.getCodigo(), 			objeto.getCodigo());
+		Assert.assertEquals(objetoBD.getId(), 				objeto.getId());
+		Assert.assertEquals(objetoBD.getNome(),				objeto.getNome());
+		
+		//Verifica o endereco
+		verificar(objetoBD.getEndereco(), 			objeto.getEndereco());
+	}
+	
+	public static void verificar(Aviao objetoBD, Aviao objeto){
+		Assert.assertEquals(objetoBD.getCodigo(), 			objeto.getCodigo());
+		Assert.assertEquals(objetoBD.getId(), 				objeto.getId());
+		Assert.assertEquals(objetoBD.getCarga(),			objeto.getCarga(), 0);
+		
+		verificar(objetoBD.getCiaAerea(), objeto.getCiaAerea());
+	}
+	
+	public static void verificar(Bagagem objetoBD, Bagagem objeto){
+		Assert.assertEquals(objetoBD.getId(), 				objeto.getId());
+		Assert.assertEquals(objetoBD.getPeso(), 			objeto.getPeso(), 0);
+		
+		verificar(objetoBD.getBilhete(), objeto.getBilhete());
+	}
+	
+	
 	public static void verificar(CiaAerea objectBD, CiaAerea object){
 		Assert.assertEquals(objectBD.getId(), 			object.getId());
 		Assert.assertEquals(objectBD.getNome(), 		object.getNome());
+	}
+	
+	public static void verificar(Bilhete objetoBD, Bilhete objeto){
+		Assert.assertEquals(objetoBD.getAssento(), 			objeto.getAssento());
+		Assert.assertEquals(objetoBD.getId(), 				objeto.getId());
+		Assert.assertEquals(objetoBD.getNumero(), 			objeto.getNumero());
+		
+		verificar(objetoBD.getHorario(), objeto.getHorario());
+		verificar(objetoBD.getPassageiro(), objeto.getPassageiro());
+	}
+	
+	
+	public static void verificar(Endereco enderecoBD, Endereco endereco){
+		Assert.assertEquals(enderecoBD.getBairro(), 		endereco.getBairro());
+		Assert.assertEquals(enderecoBD.getCidade(), 		endereco.getCidade());
+		Assert.assertEquals(enderecoBD.getComplemento(), 	endereco.getComplemento());
+		Assert.assertEquals(enderecoBD.getEstado(), 		endereco.getEstado());
+		Assert.assertEquals(enderecoBD.getId(), 			endereco.getId());
+		Assert.assertEquals(enderecoBD.getNumero(),			endereco.getNumero());
+		Assert.assertEquals(enderecoBD.getPais(), 			endereco.getPais());
+		Assert.assertEquals(enderecoBD.getRua(), 			endereco.getRua());
+	}
+	
+	public static void verificar(Funcionario objetoBD, Funcionario objeto){
+		Assert.assertEquals(objetoBD.getCodigo(), 			objeto.getCodigo());
+		Assert.assertEquals(objetoBD.getContaCorrente(), 	objeto.getContaCorrente());
+		
+		verificarPessoa(objetoBD, objeto);
+		verificar(objetoBD.getPapel(), objeto.getPapel());
+	}
+	
+	public static void verificar(Horario objetoBD, Horario objeto){
+		Assert.assertEquals(objetoBD.getCodigo(), 								objeto.getCodigo());
+		Assert.assertEquals(DateUtils.minimizeDate(objetoBD.getChegada()),	 	DateUtils.minimizeDate(objeto.getChegada()));
+		Assert.assertEquals(objetoBD.getId(), 									objeto.getId());
+		Assert.assertEquals(DateUtils.minimizeDate(objetoBD.getPartida()), 		DateUtils.minimizeDate(objeto.getPartida()));
+		Assert.assertEquals(objetoBD.getQtdEconomica(), 						objeto.getQtdEconomica());
+		Assert.assertEquals(objetoBD.getQtdExecutiva(), 						objeto.getQtdExecutiva());
+		Assert.assertEquals(objetoBD.getQtdPrimeira(), 							objeto.getQtdPrimeira());
+		
+		verificar(objetoBD.getAviao(), objeto.getAviao());
+		verificar(objetoBD.getRota(), objeto.getRota());
+	}
+	
+	public static void verificar(Papel objetoBD, Papel objeto){
+		Assert.assertEquals(objetoBD.getId(), 			objeto.getId());
+		Assert.assertEquals(objetoBD.getNome(), 		objeto.getNome());
+		Assert.assertEquals(objetoBD.getDescricao(), 	objeto.getDescricao());
+		
+		verificar(objetoBD.getCiaAerea(), objeto.getCiaAerea());
+	}
+	
+	public static void verificar(Passageiro objetoBD, Passageiro objeto){
+		Assert.assertEquals(objetoBD.getDocumento(), 		objeto.getDocumento());
+		Assert.assertEquals(objetoBD.getNumeroCartao(), 	objeto.getNumeroCartao());
+		
+		verificarPessoa(objetoBD, objeto);
+	}
+	
+	public static void verificarPessoa(Pessoa objetoBD, Pessoa objeto){
+		Assert.assertEquals(DateUtils.minimizeDate(objetoBD.getDataNascimento()), 	DateUtils.minimizeDate(objeto.getDataNascimento()));
+		Assert.assertEquals(objetoBD.getEmail(), 			objeto.getEmail());
+		Assert.assertEquals(objetoBD.getId(), 				objeto.getId());
+		Assert.assertEquals(objetoBD.getNome(),				objeto.getNome());
+		Assert.assertEquals(objetoBD.getSenha(), 			objeto.getSenha());
+		Assert.assertEquals(objetoBD.getTelefone(), 		objeto.getTelefone());
+		Assert.assertEquals(objetoBD.getUsuario(), 			objeto.getUsuario());
+		
+		//Verifica o endereco
+		verificar(objetoBD.getEndereco(), 			objeto.getEndereco());
+	}
+	
+	public static void verificar(Rota objetoBD, Rota objeto){
+		Assert.assertEquals(objetoBD.getId(), 			objeto.getId());
+		Assert.assertEquals(objetoBD.getNome(), 		objeto.getNome());
+		Assert.assertEquals(objetoBD.getDescricao(), 	objeto.getDescricao());
+		
+		verificar(objetoBD.getCiaAerea(), objeto.getCiaAerea());
+		verificar(objetoBD.getOrigem(), objeto.getOrigem());
+		verificar(objetoBD.getDestino(), objeto.getDestino());
 	}
 	
 }
