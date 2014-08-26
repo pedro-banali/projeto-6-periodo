9f5a636028712fc7ccbfb7523fde388a22c973ba
pedro <pedro@10.97.250.152>
2014-08-26 18:16:04 -0300

    Factory

--- TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/bc/BilheteBCFactory.java
+++ TicketsAereosBSI_JSF/src/main/java/br/pucpr/bsi/prog6/ticketsAereosBSI/bc/BilheteBCFactory.java
@@ -1,14 +1,27 @@
 package br.pucpr.bsi.prog6.ticketsAereosBSI.bc;
 
+import br.pucpr.bsi.prog6.ticketsAereosBSI.enums.TipoBilheteEnum;
+
 public class BilheteBCFactory {
 	
-	 //private static BilheteBCFactory instance;
+	 private static BilheteBCFactory instance;
 		
 	 public BilheteBCFactory()
 	 {
 		 
 	 }
+
+	public static BilheteBC getInstanceBC(TipoBilheteEnum tipoBilheteEnum) {
+		// TODO Auto-generated method stub
+		if(tipoBilheteEnum == TipoBilheteEnum.ECONOMICA)
+			return null;
+		else if(tipoBilheteEnum == TipoBilheteEnum.EXECUTIVA)
+			return null;
+		else
+			return null;
+	}
 	
+	 
 	 
 	 
 }
